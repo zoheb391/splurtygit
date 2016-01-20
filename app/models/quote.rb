5ffd51f2d3b676feed02cf7879251c7c8ec7ce84
Zoheb <zoheb_391@hotmail.com>
2016-01-20 23:39:01 +0000

    1st test done

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,13 @@
 class Quote < ActiveRecord::Base
 validates :saying, :presence => true, :length  => { :maximum => 140, :minimum => 3 }
 validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3 }
+
+ def unique_tag
+  abbr = []
+   self.author.split(" ").collect do |sub_string|	
+   abbr << sub_string[0]
+   end
+  abbr.join + "#" + self.id.to_s
+ end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do 
+  	quote= Quote.create(author: 'Ken Mazaika', saying: 'You only yolo once')
+  	expected = "KM#" + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
   end
 end
